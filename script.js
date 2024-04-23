40c33eb3837040b6088ce91ad66ed2ff3db8a3d4
echoo35 <135036416+echoo35@users.noreply.github.com>
2024-04-23 18:15:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+alert("Merhaba Gencler"); // Alert ile bir iletişim kutusu açar ve "Merhaba Gençler" metnini gösterir.
+
+// Tek satırlık bir yorum
+
+/* Çok satırlı yorumlar
+   Bu yorumun içinde birden fazla satır var
+   Bu yorum çoklu satırı destekler
+*/
+
+// Numbers (Sayılar)
+let length = 16; // Bir değişken oluşturulur ve değeri 16 olarak atanır.
+console.log(length); // Değişkenin değeri konsola yazdırılır.
+
+let weight = 7.5; // Bir diğer sayısal değerli değişken oluşturulur ve değeri 7.5 olarak atanır.
+console.log(weight); // Değişkenin değeri konsola yazdırılır.
+
+// Stringler
+let color = "yellow"; // Bir metinsel değerli değişken oluşturulur ve değeri "yellow" olarak atanır.
+console.log(color); // Değişkenin değeri konsola yazdırılır.
+
+let lastname = "jahnson"; // Başka bir metinsel değerli değişken oluşturulur ve değeri "jahnson" olarak atanır.
+console.log(lastname); // Değişkenin değeri konsola yazdırılır.
+
+// Booleans (Boolean Değerler)
+let x = true; // Bir boolean değişken oluşturulur ve değeri true (doğru) olarak atanır.
+console.log(x); // Değişkenin değeri konsola yazdırılır.
+
+let y = false; // Başka bir boolean değişken oluşturulur ve değeri false (yanlış) olarak atanır.
+console.log(y); // Değişkenin değeri konsola yazdırılır.
+
+// Object (Obje)
+const person = { // Bir obje oluşturulur.
+    firstname: "john", // Objeye bir özellik (firstname) ve değeri (john) atanır.
+    lastname: "doe" // Objeye bir özellik (lastname) ve değeri (doe) atanır.
+};
+console.log(person); // Obje konsola yazdırılır.
+
+// Array (Dizi)
+const cars = ["saab", "volvo", "BMW"]; // Bir dizi oluşturulur ve içine üç öğe eklenir.
+console.log(cars); // Dizi konsola yazdırılır.
+
+// Date Object (Tarih Objesi)
+const date = new Date("2022-03-25"); // Bir tarih nesnesi oluşturulur ve içine belirtilen tarih atanır.
+console.log(date); // Tarih nesnesi konsola yazdırılır.
